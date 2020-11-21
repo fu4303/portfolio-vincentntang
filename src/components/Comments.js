e35cb770cd5318f1c9f4b4acb9f96c1a89528c30
vincenttang <10290348+vincentntang@users.noreply.github.com>
2020-11-20 22:19:14 -0500

    clean up utterance to just componentDidMount only

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -6,77 +6,18 @@ export default class Comments extends Component {
   constructor(props){ 
     super(props);
     this.commentBox = React.createRef();
-    // this.scriptEl = document.createElement("script");
-    this.state = {
-      keyForceRender: 0
-    }
   }
   componentDidMount () {
-      // // this.scriptEl = document.createElement("script");
-      // this.scriptEl.setAttribute("src", "https://utteranc.es/client.js");
-      // this.scriptEl.setAttribute("crossorigin","anonymous");
-      // this.scriptEl.setAttribute("async", true);
-      // this.scriptEl.setAttribute("repo", "vincentntang/vincentntang.com-comments");
-      // this.scriptEl.setAttribute("issue-term", "pathname");
-      // this.scriptEl.setAttribute( "theme", "github-dark");
-      
-      // this.commentBox.current.appendChild(this.scriptEl);
-
+      const theme = this.context;
+      const utteranceTheme = theme.dark ? "github-dark" : "github-light";
       let scriptEl = document.createElement("script");
       scriptEl.setAttribute("src", "https://utteranc.es/client.js");
       scriptEl.setAttribute("crossorigin","anonymous");
       scriptEl.setAttribute("async", true);
       scriptEl.setAttribute("repo", "vincentntang/vincentntang.com-comments");
       scriptEl.setAttribute("issue-term", "pathname");
-      scriptEl.setAttribute( "theme", "github-dark");
+      scriptEl.setAttribute( "theme", utteranceTheme);
       this.commentBox.current.appendChild(scriptEl);
-  }
-
-  async componentDidUpdate(previousProps, previousState) {
-    console.log(this.state.keyForceRender,"KeyForceRender");
-    console.log(previousState.keyForceRender,"KeyForceRender prev State")
-
-    if(previousState.keyForceRender !== this.state.keyForceRender) {
-      return;
-    }
-    const theme = this.context;
-    if(theme.dark){
-      let scriptEl = document.createElement("script");
-      scriptEl.setAttribute("src", "https://utteranc.es/client.js");
-      scriptEl.setAttribute("crossorigin","anonymous");
-      scriptEl.setAttribute("async", true);
-      scriptEl.setAttribute("repo", "vincentntang/vincentntang.com-comments");
-      scriptEl.setAttribute("issue-term", "pathname");
-      scriptEl.setAttribute( "theme", "github-dark");
-      this.commentBox.current.textContent = '';
-      this.commentBox.current.appendChild(scriptEl);
-      await this.setState(prevState => ({
-        keyForceRender: prevState.keyForceRender + 1
-      }))
-    } else {
-      let scriptEl = document.createElement("script");
-      scriptEl.setAttribute("src", "https://utteranc.es/client.js");
-      scriptEl.setAttribute("crossorigin","anonymous");
-      scriptEl.setAttribute("async", true);
-      scriptEl.setAttribute("repo", "vincentntang/vincentntang.com-comments");
-      scriptEl.setAttribute("issue-term", "pathname");
-      scriptEl.setAttribute( "theme", "github-light");
-      this.commentBox.current.textContent = '';
-      this.commentBox.current.appendChild(scriptEl);
-      await this.setState(prevState => ({
-        keyForceRender: prevState.keyForceRender + 1
-      }))
-    }
-    // if(theme.dark){
-    //   console.log(this.commentBox,"CommentBox")
-    //   this.scriptEl.setAttribute("theme", "github-dark")
-    //   this.commentBox.current.appendChild(this.scriptEl);
-    //   this.keyForceRender++;
-    // } else {
-    //   this.scriptEl.setAttribute("theme", "github-light")
-    //   this.commentBox.current.appendChild(this.scriptEl);
-    //   this.keyForceRender++;
-    // }
   }
 
   render() {
@@ -84,7 +25,7 @@ export default class Comments extends Component {
         <div className="comment-box-wrapper container pt-7">
           <h1 className="mb-0">Comments</h1>
           <hr className="my-0"/>
-          <div key={this.keyForceRender} ref={this.commentBox} className="comment-box"></div>
+          <div ref={this.commentBox} className="comment-box"></div>
         </div>
     );
   }
